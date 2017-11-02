9af294453378a8e7ade4c53add065f031db51776
Mykhailo Maryn <mymar@opway.com>
2017-11-02 11:44:00 +0200

    removed app.js and move functionality to /container/app.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,19 +1,37 @@
-import React from "react";
-import { combineReducers, createStore, applyMiddleware, compose } from "redux";
-import { Provider } from "react-redux";
+import React, { Component } from "react";
+import { combineReducers, createStore, applyMiddleware, bindActionCreators, compose } from "redux";
+import { Provider, connect } from "react-redux";
 import thunk from "redux-thunk";
 
-import AnimatedPicker from "./AnimatedPicker";
 import rootsReducers from "../reducers/_indexReducer";
 
-const rootReducer = combineReducers(rootsReducers);
+import Actions from "../actions/_indexAction";
+import Home from "../components/Home";
 
-//const store = createStore(rootReducer, applyMiddleware(thunk));
+class AnimatedPicker extends Component {
+  render() {
+    return (
+      <Home store={this.props.store} actions={this.props.actions} />
+    )
+  }
+}
+
+const mapStateToProps = state => ({
+  store: state
+});
+
+const mapActionsToProps = dispatch => ({
+  actions: bindActionCreators(Actions, dispatch)
+});
+
+const AnimatedPickerContainer = connect(mapStateToProps, mapActionsToProps)(AnimatedPicker);
+
+const rootReducer = combineReducers(rootsReducers);
 const store = compose(applyMiddleware(thunk))(createStore)(rootReducer);
 
 const App = () => (
   <Provider store={store}>
-    <AnimatedPicker />
+    <AnimatedPickerContainer />
   </Provider>
 );
 
